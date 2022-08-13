5c23fb9862130c4e10ba09e63b0b73fa7137be33
KCBABY620 <111185657+KCBABY620@users.noreply.github.com>
2022-08-13 23:06:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wx7a0d8f20e5cc8723"
 # 公众号appSecret
-app_secret = ""
+app_secret = "439e1c6a4021e3c497ed21144b5d83e5"
 # 模板消息id
-template_id = ""
+template_id = "JB5ZseTP9vnfRoOzv87nS7NRW0u_f_gBdZFtawvvVIY"
 # 接收公众号消息的微信号，如果有多个，需要用英文逗号间隔
-user = [""]
+user = ["o3eVy6r3TUJ98t8sPztxnwf4af-4"]
 
 # 信息配置
 # 所在省份
 province = "广东"
 # 所在城市
-city = "广州"
+city = "深圳"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1997-1-1"
+birthday = "1985-6-20"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2022-6-7"
 
